e643d41cb8472a4d4dee1a556cf9dfc03db98afb
Sadman Alam Reshad <sadman.alam@bjitgroup.com>
2022-12-27 14:09:10 +0600

    Push 3
    2:09 PM, 27 December, 2022

--- TalkToMe/app/src/main/java/com/r3denvy/talktome/activities/MainActivity.kt
+++ TalkToMe/app/src/main/java/com/r3denvy/talktome/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.r3denvy.talktome.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
